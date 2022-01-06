62aa44beca8811892bb333cd3b965d435c8f9780
no-do-not-track-me <no-do-not-track-me@gmail.com>
2022-01-07 00:47:42 +0530

    problem solved

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -7,8 +7,24 @@
 
 import UIKit
 
+class Solution {
+	func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+		var hashMap: [Int: Int] = [: ]
+		for i in 0..<nums.count {
+			let expected = target - nums[i]
+			if let index = hashMap[expected] {
+				return [index, i]
+			} else {
+				hashMap[nums[i]] = i
+			}
+		}
+		return [0, 0]
+	}
+}
+
 class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		print(Solution().twoSum([2,7,11,15], 9))
 	}
 }
