1f57853b95b6146b683fe732b3c1a1f4d72f276c
Sagar Kothari <sag333ar@gmail.com>
2022-01-08 19:57:27 +0530

    Problem solved

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
+class Solution {
+	func isValid(_ s: String) -> Bool {
+		let bracketsMap: [Character: Character] = ["}": "{", ")": "(", "]": "["]
+		guard let openingBrackets = bracketsMap.values else { return false }
+		let problem = Array(s)
+		var stack: [Character] = []
+		if !openingBrackets.contains(problem[0]) {
+			print("String starts with closing bracket.")
+			return false
+		}
+		for i in 1..<problem.count {
+			if openingBrackets.contains(problem[i]) {
+				stack.append(problem[i])
+			} else if let last = stack.last, let openingForCurrent = bracketsMap[problem[i]] {
+				if openingForCurrent == last {
+					stack.popLast()
+				} else {
+					return false
+				}
+			}
+		}
+		return stack.isEmpty
+	}
+}
+
 class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
