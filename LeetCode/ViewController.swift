01646e3ed0c015284fda3d3da300c97097707979
Sagar Kothari <sag333ar@gmail.com>
2022-01-08 16:58:11 +0530

    Problem solved

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -7,8 +7,34 @@
 
 import UIKit
 
+class Solution {
+	func longestCommonPrefix(_ strs: [String]) -> String {
+		var prefix = ""
+		var charIndex = 0
+		while charIndex < strs[0].count {
+			let firstChar = Array(strs[0])[charIndex]
+			for i in 1..<strs.count {
+				if charIndex >= strs[i].count {
+					return prefix
+				}
+				let char = Array(strs[i])[charIndex]
+				if firstChar != char {
+					return prefix
+				}
+			}
+			prefix = "\(prefix)\(firstChar)"
+			charIndex += 1
+		}
+		return prefix
+	}
+}
+
 class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		print("Longest Common Prefix for [\"flower\",\"flow\",\"flight\"] is - ")
+		print((Solution().longestCommonPrefix(["flower","flow","flight"])))
+		print("Longest Common Prefix for [\"dog\",\"racecar\",\"car\"] is - ")
+		print((Solution().longestCommonPrefix(["dog","racecar","car"])))
 	}
 }
