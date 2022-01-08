30eff0d109789fad10bfa42b869b261b40081421
no-do-not-track-me <no-do-not-track-me@gmail.com>
2022-01-08 11:35:38 +0530

    added solution

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -7,8 +7,42 @@
 
 import UIKit
 
+class Solution {
+	let roman: [Character: Int] = [
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000
+	]
+
+	func romanToInt(_ s: String) -> Int {
+		var result = 0
+		let chars = Array(s)
+		for i in 0..<chars.count {
+			if let charValue = roman[chars[i]] {
+				if i >= s.count - 1 {
+					result += charValue
+				} else if let nextCharValue = roman[chars[i + 1]] {
+					if charValue < nextCharValue {
+						result -= charValue
+					} else {
+						result += charValue
+					}
+				}
+			}
+		}
+		return result
+	}
+}
+
 class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		print("III = \(Solution().romanToInt("III"))")
+		print("LVIII = \(Solution().romanToInt("LVIII"))")
+		print("MCMXCIV = \(Solution().romanToInt("MCMXCIV"))")
 	}
 }
